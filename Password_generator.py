971e1353ea786e311ecf63a7540ca68ad4ec0da4
Bocchetty <bocchettifrancesco06@outlook.it>
2021-08-01 16:31:43 +0200

    L'inizio

---
+++ Password_generator.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+print('Hello, welcome to password generator')
+
+length= int(input("Enter the lenght of the password:"))
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+all = lower + upper + num + symbols
+
+temp = random.sample(all,length)
+password = "".join(temp)
+
+print(password)

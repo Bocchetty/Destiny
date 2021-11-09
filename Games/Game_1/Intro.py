fae1a49ed34d46cd8b13619c52dfd0e8a5b0bacd
Bocchetty <bocchettifrancesco06@outlook.it>
2021-11-10 00:29:21 +0100

    prima versione

--- Games/Game_1/Intro.py
+++ Games/Game_1/Intro.py
@@ -1,2 +1,30 @@
-import pgzrun
+#Questo gioco è dedicato a Vicky pazza SGRAVATA
 
+import pgzrun
+from pgzero.builtins import Actor, animate, keyboard
+
+penguin = Actor('icon_penguin')
+#penguin.topright = 0, 10
+
+WIDTH = penguin.width + 20
+HEIGHT = penguin.height + 20
+
+def draw():
+    screen.clear()
+    penguin.draw()
+
+score= 0
+
+def on_mouse_down(pos):
+    global score
+    if penguin.collidepoint(pos):
+        #sounds.eep.play()
+        penguin.image = 'penguin_hurt'
+        score += 1
+        print("Eek")
+    else:
+        score -= 1
+        print("Nothing here bu")
+    print(score)
+    
+pgzrun.go()

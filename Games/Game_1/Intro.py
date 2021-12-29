e40831e6f2368d779b4a17b9c01759dbf05c72c9
Bocchetty <bocchettifrancesco06@outlook.it>
2021-12-29 16:49:42 +0100

    Migioramenti al movimento

--- Games/Game_1/Intro.py
+++ Games/Game_1/Intro.py
@@ -6,13 +6,15 @@ from pgzero.builtins import Actor, animate, keyboard
 from pygame.locals import *
 
 penguin = Actor('penguin', bottomleft = (0, 600))
-penguin2= Actor('penguin2', (700, 550))
-rock= Actor('rock', (400, 550))
+penguin2 = Actor('penguin2', (700, 550))
+rock = Actor('rock', (400, 550))
 
 background = pygame.image.load("Games\Game_1\Images\\background.png")
 
 WIDTH = 800
 HEIGHT = 600
+
+penguin.speed = 5
 
 score= 0
 
@@ -26,15 +28,15 @@ def draw():
     rock.draw()
 
 def update():
-    if keyboard.left:
+    if keyboard.left or keyboard.a:
         penguin.x-=1
-        set_penguin_left()
-    elif keyboard.right:
+        set_penguin_walk_left()
+    elif keyboard.right or keyboard.d:
         penguin.x+=1
-        set_penguin_normal()
-    if keyboard.up:
+        set_penguin_walk()
+    if keyboard.up or keyboard.w:
         penguin.y-=1
-    elif keyboard.down:
+    elif keyboard.down or keyboard.s:
         penguin.y+=1
 
 def on_mouse_down(pos):
@@ -58,6 +60,38 @@ def set_penguin_hit():
 def set_penguin_normal():
     penguin.image = "penguin"
     
+def set_penguin_walk():
+    penguin.image = "penguin_walk"
+    clock.schedule_unique(set_penguin_walk_2, 0.3)
+    
+def set_penguin_walk_2():
+    penguin.image = "penguin_walk_2"
+    clock.schedule_unique(set_penguin_walk_3, 0.3)
+    
+def set_penguin_walk_3():
+    penguin.image = "penguin_walk_3"
+    clock.schedule_unique(set_penguin_walk_4, 0.3)
+
+def set_penguin_walk_4():
+    penguin.image = "penguin_walk_4"
+    clock.schedule_unique(set_penguin_walk, 0.3)
+
+def set_penguin_walk_left():
+    penguin.image = "penguin_walk_left"
+    clock.schedule_unique(set_penguin_walk_2_left, 0.3)
+    
+def set_penguin_walk_2_left():
+    penguin.image = "penguin_walk_2_left"
+    clock.schedule_unique(set_penguin_walk_3_left, 0.3)
+    
+def set_penguin_walk_3_left():
+    penguin.image = "penguin_walk_3_left"
+    clock.schedule_unique(set_penguin_walk_4_left, 0.3)
+    
+def set_penguin_walk_4_left():
+    penguin.image = "penguin_walk_4_left"
+    clock.schedule_unique(set_penguin_walk_left, 0.3)
+
 def set_penguin_left():
     penguin.image = "penguin_left"
 

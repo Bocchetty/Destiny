0e9ccfb4017dfde42d3e53cbc97138ae43b7179c
Bocchetty <bocchettifrancesco06@outlook.it>
2022-02-15 22:33:02 +0100

    Create auto_write_bot.py

---
+++ auto_write_bot.py
@@ -0,0 +1,10 @@
+import pyautogui as pg
+import time
+
+print ("Program in 5 seconds...")
+time.sleep (10)
+
+for i in range(100):
+    pg.write (" Ti amo ")
+    time.sleep (0.5)
+    pg.press ("Enter")

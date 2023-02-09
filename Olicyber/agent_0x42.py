a1703bf16734dc8653dbf3ec71dd1251dba2af9c
Bocchetty <bocchettifrancesco06@outlook.it>
2023-02-09 14:35:18 +0100

    Update agent_0x42.py

--- Olicyber/agent_0x42.py
+++ Olicyber/agent_0x42.py
@@ -1,7 +1,10 @@
 #Devo ottenere la chiave di decifrazione
 
 data = "42656e76656e75746f206167656e746520307834322c206c6120636f6d756e6963617a69"
+data2 = "42656e76656e75746f206167656e74652c206964656e7469666963617469206369667261"
 
 decr = bytes.fromhex(data)
+decr2 = bytes.fromhex(data2)
 
 print(decr)
+print(decr2)

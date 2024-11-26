828928dc773ec8838e893e7f3942415c2d727be6
Bocchetty <bocchettifrancesco06@outlook.it>
2024-11-26 16:08:47 +0100

    completamento esercizio

--- università/fondamenti_di_informatica/menu_vettore_no_pointers.c
+++ università/fondamenti_di_informatica/menu_vettore_no_pointers.c
@@ -1,6 +1,7 @@
 //Menù contestuale per effettuare operazioni su array inseriti da tastiera
 
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAXDIM 100
 
@@ -35,8 +36,8 @@ int main(){
     switch(scelta){
 
         case 1:
-            stampa(v, riemp);
-            break;
+         stampa(v, riemp);
+         break;
 
         case 2:
          printf("Scrivi il valore da inserire\n");
@@ -49,16 +50,29 @@ int main(){
          break;
 
         case 3:
-         printf("Scrivi l'elemento da eliminare\n");
+         printf("Scrivi l'indice dell'elemento da eliminare\n");
          scanf("%d", &valore);
+         ris = eliminazione(v, riemp, valore);
+         if (ris != -1)
+             riemp = ris; //Aggiorno il riempimento dopo l'eliminazione
+         else
+          printf("Elemento da eliminare non trovato!");
+         break;
         
-
         case 4:
-
+         printf("Scrivi l'elemento che vuoi ricercare\n");
+         scanf("%d", &valore);
+         ris = ricerca(v, riemp, valore);
+         if (ris == -1){
+            printf("Valore non trovato!\n");
+         }else{
+         printf("L'indice in cui si trova il valore e' %d\n", ris);
+         }
+         break;
 
         default:
             if (scelta!= 0)
-                printf("Scelta non consentita!\n");
+             printf("Scelta non consentita!\n");
             else
              break;
     }
@@ -82,7 +96,7 @@ void stampa(int v[], int riemp){
 int inserimento(int v[], int riemp, int valore){
 
     if (riemp < MAXDIM){
-     v[riemp]= valore; //Inserisco il valore all'indice attuale del riempimento
+     v[riemp] = valore; //Inserisco il valore all'indice attuale del riempimento
      riemp ++;
      return riemp;
 
@@ -91,7 +105,35 @@ int inserimento(int v[], int riemp, int valore){
     }
 }
 
-int eliminazione(int v[], int riemp, int valore_da_eliminare){
+int eliminazione(int v[], int riemp, int valore){
 
+    if (valore == -1)
+        return -1;
+    else{
+     riemp--;
+     for(int i = valore; i < riemp; i++)
+     v[i] = v[i+1];
+    }
 
+    return riemp;
+
+}
+
+int ricerca(int v[], int riemp, int valore){
+
+    bool valore_trovato = false;
+    int i = 0;
+    while( i < riemp && valore_trovato == false){
+
+         if(valore == v[i]){
+             valore_trovato = true;
+         }else{
+             i++;
+         }
+    }
+    if (valore_trovato == true){
+        return i;
+    }else{
+        return -1;
+    }
 }

3f478e07c4fb8e4979d5cd7d219dee0ab34256f1
Bocchetty <bocchettifrancesco06@outlook.it>
2024-12-10 16:53:01 +0100

    implementazione funzione

--- università/fondamenti_di_informatica/puntatori_swap.c
+++ università/fondamenti_di_informatica/puntatori_swap.c
@@ -3,14 +3,16 @@
 #include <stdio.h>
 #include<stdlib.h>
 
+void swap_ptr(int *pa, int *pb);
+
 int main(){
 
     int a = 0;
     int b = 0;
-    int temp = 0;
+    //int temp = 0;
     int *pa = &a;
     int *pb = &b;
-    int *pt = &temp;
+    //int *pt = &temp;
 
     printf("Inserisci due interi:\n");
     scanf("%d", pa);
@@ -19,12 +21,25 @@ int main(){
     printf("Prima dello scambio A=%d, B=%d\n", *pa, *pb);
     printf("Indirizzo di A: %p\n", pa);
     printf("Indirizzo di B: %p\n", pb);
+    /*
     *pt = *pa;
     *pa = *pb;
     *pb = *pt;
+    */
+    swap_ptr(pa, pb);
+
     printf("Dopo lo scambio A=%d, B=%d\n", *pa, *pb);
     printf("Indirizzo di A: %p\n", pa);
     printf("Indirizzo di B: %p\n", pb);
 
     return 0;
+}
+
+void swap_ptr(int *pa, int *pb){
+
+    int temp= 0;
+
+    temp = *pa;
+    *pa = *pb;
+    *pb = temp;
 }

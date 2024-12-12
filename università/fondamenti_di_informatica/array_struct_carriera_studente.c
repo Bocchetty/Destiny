979f790c53754894048a7eec71bf4f0d2aa701b4
Bocchetty <bocchettifrancesco06@outlook.it>
2024-12-12 23:40:25 +0100

    inizio nuovo esercizio

---
+++ università/fondamenti_di_informatica/array_struct_carriera_studente.c
@@ -0,0 +1,187 @@
+//Questo programma manipola un'array di strutture studente con le relative informazioni
+
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+#define MAXLEN 50
+#define MAXDIM 100
+
+struct studente{
+
+    char nome[MAXLEN];
+    char cognome[MAXLEN];
+    int eta;
+    float media;
+    bool lavoratore;
+
+};
+
+struct studente studenti[MAXDIM];
+
+void flush();
+void inserisci_studente(struct studente *studenti, int *riemp);
+void stampa(struct studente *studenti, int riemp, char *nome_cercato, char *cognome_cercato);
+int eta(struct studente *studenti, int riemp, char *nome_cercato, char *cognome_cercato);
+
+int main(){
+
+    int scelta = 0;
+    int riemp = 0;
+    char nome_cercato[MAXLEN];
+    char cognome_cercato[MAXLEN];
+    int ris = 0;
+
+    do {
+    
+    printf("Menu' interattivo\n");
+    printf("1. Inserisci studente\n");
+    printf("2. Stampa studente dato nome e cognome\n");
+    printf("3. Eta' dati nome e cognome\n");
+    printf("4. Media studenti non lavoratori\n");
+    printf("5. Stampa studenti con eta' inferiore a 25 anni\n");
+    printf("0. Esci\n");
+    printf("Quale operazione vuoi eseguire?\n");
+    scanf("%d", &scelta);
+    flush();
+
+    switch (scelta) {
+
+        case 1:
+        inserisci_studente(studenti, &riemp);
+        break;
+
+        case 2:
+        stampa(studenti, riemp, nome_cercato, cognome_cercato);
+        break;
+
+        case 3:
+        ris = eta(studenti, riemp, nome_cercato, cognome_cercato);
+        if(ris != -1){
+            printf("L'eta' di %s %s e' %d\n", nome_cercato, cognome_cercato, ris);
+        };
+        break;
+
+        case 4:
+        
+        break;
+
+        case 5:
+
+        break;
+
+        case 0:
+        printf("Arrivederci\n");
+        break;
+
+        default:
+        printf("Valore inserito non valido!\n");
+        break;
+    
+    }
+
+    }while (scelta != 0);
+
+    return 0;
+}
+
+void flush(){
+
+    int c;
+    while((c = getchar()) != '\n' && c != EOF);
+}
+
+void inserisci_studente(struct studente *studenti, int *riemp){
+
+    if((*riemp + 1) < MAXDIM){
+    
+     int lun = 0;
+     char lavoro;
+     printf("Inserisci dati studente\n");
+     printf("Inserisci NOME:\n");
+     fgets((studenti + *riemp) -> nome, MAXLEN, stdin);
+
+     lun = strlen((studenti + *riemp) -> nome);
+     (studenti + *riemp) -> nome[lun-1] = '\0';
+
+     printf("Inserisci COGNOME:\n");
+     fgets((studenti + *riemp) -> cognome, MAXLEN, stdin);
+
+     lun = strlen((studenti + *riemp) -> cognome);
+     (studenti + *riemp) -> cognome[lun-1] = '\0';
+
+     printf("Inserisci ETA'\n");
+     scanf("%d", &(studenti + *riemp) -> eta);
+     printf("Inserisci MEDIA\n");
+     scanf("%f", &(studenti + *riemp) -> media);
+     printf("Studente LAVORATORE? (s/n)\n");
+     scanf("%s", &lavoro);
+
+     if(lavoro == 's'){
+         (studenti + *riemp) -> lavoratore = true;
+     }else{
+         (studenti + *riemp) -> lavoratore = false;
+     }
+
+     (*riemp)++;
+     flush();
+    }else {
+    return;
+    }
+}
+
+void stampa(struct studente *studenti, int riemp, char *nome_cercato, char *cognome_cercato){
+
+    bool trovato = false; 
+    printf("Inserisci il nome dello studente che cerchi\n");
+    scanf("%s", nome_cercato);
+    printf("Inserisci il cognome dello studente che cerchi\n");
+    scanf("%s", cognome_cercato);
+    flush();
+    printf("\n");
+
+    for(int i = 0; i < riemp; i++){
+
+        if((strcmp((studenti + i)->nome, nome_cercato) == 0) && (strcmp((studenti + i)->cognome, cognome_cercato) == 0)){
+
+            trovato = true;
+            printf("Studente trovato!\n");
+            printf("%s\n", (studenti + i)->nome);
+            printf("%s\n", (studenti + i)->cognome);
+            printf("%d\n", (studenti + i)->eta);
+            printf("%0.2f\n", (studenti + i)->media);
+            if(((studenti + i)->lavoratore) == 1){
+                printf("Lavoratore\n");
+            }else {
+                printf("Non lavoratore\n");
+            }
+            
+        }
+    }
+    if(trovato == false){
+    printf("Studente non trovato!\n");
+    }
+}
+
+int eta(struct studente *studenti, int riemp, char *nome_cercato, char *cognome_cercato){
+    
+    bool trovato = false; 
+    printf("Inserisci il nome dello studente di cui cerchi l'eta'\n");
+    scanf("%s", nome_cercato);
+    printf("Inserisci il cognome dello studente di cui cerchi l'eta'\n");
+    scanf("%s", cognome_cercato);
+    flush();
+    printf("\n");
+
+    for(int i = 0; i < riemp; i++){
+
+        if((strcmp((studenti + i)->nome, nome_cercato) == 0) && (strcmp((studenti + i)->cognome, cognome_cercato) == 0)){
+
+            trovato = true;
+            return (studenti + i)->eta;
+        }
+    }
+    if(trovato == false){
+    printf("Studente non trovato!\n");
+    return -1;
+    }
+}
